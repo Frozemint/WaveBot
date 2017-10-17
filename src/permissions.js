2eae2b12c5a7b54c2b6a0af148a2f0a3c5e88287
Nami <nami5504@gmail.com>
2017-10-17 09:07:09 -0700

    updated permissions checking to be more verbose

--- src/permissions.js
+++ src/permissions.js
@@ -1,11 +1,11 @@
 function checkPermissions(message){
 	if (message.member.roles.find('name', 'Bot Commander') || message.author.id === '114721723894595589' || message.member.hasPermission('ADMINISTRATOR')) {
 		//Check if use has role "Bot Commander" tagged or if writer of bot is trying ot run command.
-		console.log(Date() + ': User ' + message.author.username + ' is admin, running command.');
+		console.log(Date() + ': User ' + message.author.tag + ' is admin, running command.');
 		return true;
 	} else {
 		//Log the incident if it's not an authorised user.
-		console.log('User ' + message.author.username + ' just attempted to run a admin only command and was denied.');
+		console.log('User ' + message.author.tag + ' just attempted to run a admin only command and was denied.');
 		message.channel.send(':no_entry_sign: | You do not have permission to execute the said command, this incident will be reported.');
 		return false;
 	}
