c3107f95713e6783286437cfc37210cb99e3f726
Nami <nami5504@gmail.com>
2018-01-01 18:15:55 -0800

    Added exit handler...

--- src/bot.js
+++ src/bot.js
@@ -4,32 +4,25 @@ var configFile = require('../config/config.json');
 var commands = require('./commands.js');
 commands = commands.commands;
 
-process.stdin.resume();
-
-function exitHandler(option, err){
-	console.log('Process exiting at: ' + Date());
-	client.destroy();
-	if (err) {
-		console.log('Time is now: ' + Date());
-		console.log('Error stack: \n\n' + err.stack);
-	}
-	if (option.exit) {
-		process.exit();
-	}
-}
-
-process.on('exit', exitHandler.bind(null,{exit:true}));
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+process.title = 'wavebot';
+process.on('exit', (code) => { console.log('Bot exiting ' + ((code > 0) ? 'with error' : 'normally') + ' at time: ' + Date()); });
+process.on('SIGINT', () => { console.log('Caught ^C, exiting at time: ' + Date()); });
+process.on('uncaughtException', (err) => { 
+	console.log('UNCAUGHT EXCEPTION at time ' + Date() + '\n' + ((err && err.stack) ? err.stack : err) + '\n');
+	process.exit(1);
+ });
 
 client.on('ready', () =>{
-	console.log('Bot ready.');
+	consodle.log('Bot now online. Time is ' + Date());
 })
 
 client.on('message', msg =>{
 	if (1 == 1 && !msg.author.bot) msg.react('🎉');  //festive boi
 	if (!msg.content.startsWith(configFile.commandPrefix) || msg.author.bot) return;
-	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(configFile.commandPrefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(configFile.commandPrefix.length).split(' ')[0]](msg);
+	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(configFile.commandPrefix.length).split(' ')[0])) {
+		console.log(Date() + '| Running command "' + msg.content + '" sent by ' + msg.author.tag);
+		commands[msg.content.toLowerCase().slice(configFile.commandPrefix.length).split(' ')[0]](msg);
+	}
 })
 
 client.login(configFile.botToken);
